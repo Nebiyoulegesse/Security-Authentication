8cb155d62410c7e386f73e99c3de3d765b95f605
Nebiyou <nebiyoulegesse@gmail.com>
2020-08-20 15:51:09 -0400

    Updated with MD5 Hashing

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const md5 = require('md5');
 
-const encrypt = require("mongoose-encryption");
 const app = express();
 
 
@@ -28,7 +28,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
 
 
 const User = new mongoose.model("User", userSchema);
@@ -49,7 +49,7 @@ app.post("/register", function(req, res) {
 
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
